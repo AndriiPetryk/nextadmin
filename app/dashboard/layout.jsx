4c8669f0838104c86b17454668bffc6d6dce9d70
Andrii P <andrii.petryk@jda.com>
2024-03-12 14:24:33 -0400

    added dashboard, users, login functionality

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+import Dashboard from "@/app/dashboard/page";
+import Navbar from "@/app/ui/dashboard/navbar/navbar";
+import styles from '../ui/dashboard/dashboard.module.css';
+
+const Layout = ({children}) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.menu}>
+                <Dashboard/>
+            </div>
+            <div className={styles.content}>
+                <Navbar/>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
